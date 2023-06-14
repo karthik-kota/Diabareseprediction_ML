b0306efc168665952cd4ccc0157267b6a77c91ba
karthik-kota <karthikkota153@gmail.com>
2023-06-14 16:21:35 +0530

    jgjg

--- app.py
+++ app.py
@@ -27,13 +27,46 @@ def predict():
         result = model.predict([data])
         print(result)
         if result[0] == 0:
-            outcome = 'No Diabetics'
+            outcome = 'Negative'
         else:
-            outcome = 'Diabetic Patient'
+            outcome = 'Postive'
         print('Data has been inserted')
         return jsonify({'message':outcome})
     else:
         return render_template('predict.html')
+#=========================================show patient==============
+
+@app.route("/show-patient", methods=['GET','POST'])
+def patient_show():
+    conn = sqlite3.connect('patient.db')
+    cur = conn.cursor()
+    cur.execute('select * from PATIENT_DETAILS')
+    #print(cur.fetchall())
+    data=[]
+    for i in cur.fetchall():
+        patient={}
+        patient['patient_name'] = i[0]
+        patient['patient_age'] = i[1]
+        patient['gender'] = i[2]
+        patient['diabetic'] = i[3]
+        data.append(patient)
+    print(data)
+    return render_template('showpatient.html',data = data)
+#===========================================================================
+# @app.route("/add-patient",methods=['GET','POST'])
+# def addpatient():
+#     if request.method=='POST':
+#         conn = sqlite3.connect('patient.db')
+#         cur = conn.cursor()
+#         customername=request.form.get('Patient_Name')
+#         customeraddr=request.form.get('')
+#         customeremail=request.form.get('email')
+#         cur.execute(f"insert into customer(customer_name,customer_addr,customer_email) values('{customername}','{customeraddr}','{customeremail}')")
+#         conn.commit()
+#         print('Data as been Inserted')
+#         return jsonify({'message':'sucessfull'})
+#     else:
+#         return render_template('addcustomer.html')
 
 
 if __name__ == '__main__':
